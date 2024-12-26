9bd369174c6d8c72f1ac644ca70553d2c106f5dd
DaffaRezwazahir77 <20230810140@uniku.ac.id>
2024-12-26 21:33:36 +0700

    Add files via upload

---
+++ jsM43.js
@@ -0,0 +1,12 @@
+function pesan() {
+    alert("ini latihan java script pertama saya");
+}
+
+function pesanConsole() {
+    console.log("it's work");
+}
+
+function pesanDok(){
+document.write("Succes<br>");
+document.write("<a href='m4-2.html'>Kembali");
+}
---
+++ latihan.js
@@ -0,0 +1,4 @@
+function tampilkanNama() {
+    const nama = document.getElementById("nama").value;
+    document.getElementById("nama_anda").value = nama;
+}
